256e5c28612300da1f40e5cedd71b5fb0a24c1eb
Richard Bång <richard.bang@live.se>
2017-08-01 12:57:25 +0200

    bla gjolt

--- footer.php
+++ footer.php
@@ -7,7 +7,31 @@
 ?>
     </div>
     <footer>
-
+        <div class="max-width footer">
+            <div class="footer-item">
+                <div class="footer-logo">
+                    <p>E-sport<span>365</span></p>
+                </div>
+                <p>We provide our visitors with the best information and tips when it comes to betting on e-sports whilst giving the best bonuses and deals from the leading betting companies.</p>
+            </div>
+            <div class="footer-item">
+                <h3>E-sports</h3>
+                <a href="<?php echo esc_url(home_url('/csgo')); ?>">CSGO</a>
+                <a href="<?php echo esc_url(home_url('/dota')); ?>">DOTA</a>
+                <a href="<?php echo esc_url(home_url('/lol')); ?>">LOL</a>
+                <a href="<?php echo esc_url(home_url('/other')); ?>">Other</a>
+            </div>
+            <div class="footer-item">
+                <h3>Contact us</h3>
+                <a href="mailto:info@e-sport356.com">info@e-sport356.com</a>
+            </div>
+            <div class="footer-item">
+                <h3>How to bet on:</h3>
+                <a href="#">CSGO</a>
+                <a href="#">DOTA</a>
+                <a href="#">LOL</a>
+            </div>
+        </div>
     </footer>
     <?php wp_footer(); ?>
 <script async src="https://static.apester.com/js/sdk/v2.0/apester-javascript-sdk.min.js"></script>
---
+++ page-other.php
@@ -0,0 +1,30 @@
+<?php get_header() ; ?>
+
+<section class="single-section home-section">
+    <div class="max-width small-width">
+        <div class="other-list">
+            <?php if( have_rows('others') ): ?>
+                <?php while( have_rows('others') ) : the_row();?>
+                <div class="other-list-item">
+                    <h3><?php the_sub_field('others-headline') ; ?></h3>
+                    <?php if( have_rows('other') ): ?>
+                    <div class="bonuses">
+                        <?php while( have_rows('other') ) : the_row();?>
+                            <div class="bonus-grid">
+                            <a target="_blank" href="<?php the_sub_field('other-link') ; ?>" class="absolute-link"></a>
+                            <div class="bonus-logo">
+                                <img src="<?php the_sub_field('other-logo') ; ?>" />
+                            </div>
+                            <p><?php the_sub_field('other-bonus') ; ?></p>
+                        </div>
+                        <?php endwhile; ?>
+                    </div>
+                    <?php endif; ?>
+                </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+      </div>
+    </div>
+</section>
+
+<?php get_footer() ; ?>
